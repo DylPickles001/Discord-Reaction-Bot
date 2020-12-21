3d198fc35a9ac70fc994d2411c3be5032cb2aa2b
DylPickles001 <nguyen.dylan142@gmail.com>
2020-12-21 05:05:03 +0000

    Version 1 (based on Anson the Developer's tutorial)- *ISSUE

--- main.py
+++ main.py
@@ -1,1 +1,42 @@
 import discord
+import os
+from keep_alive import keep_alive
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+  print('We have logged in as {0.user}'.format(client))
+
+#action by bot
+@client.event
+async def on_raw_reaction_add(payload):
+  message_id = payload.message_id
+  if message_id == 790322873851052092:
+    guild_id = payload.guild_id
+    guild = discord.utils.find(lambda g: g.id == guild_id, client.guilds)
+
+    if payload.emoji.name == 'ultimate':
+      role = discord.utils.get(guild.roles, name='ultimate')
+    elif payload.emoji.name == 'melee':
+      role = discord.utils.get(guild.roles, name='melee')
+    else:
+      role = discord.utils.get(guild.roles, name= payload.emoji.name)  
+
+    if role is not None:
+      member = discord.utils.find(lambda m : m.id == payload.user_id, guild.members)
+      if member is not None:
+        await member.add_roles(role)
+        print('done')
+      else:
+        print('Member not found')
+    else: 
+      print('Role not found')
+
+@client.event
+async def on_raw_reaction_remove(payload):
+  pass
+
+#runs bot
+keep_alive()
+client.run(os.getenv('TOKEN'))
